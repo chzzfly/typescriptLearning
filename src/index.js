b2db73759a24d4af9adba1e0ca29c283595e8381
chzzXx13 <mail@chzzfly.com>
2023-02-05 18:12:35 +0800

    interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("hello,typescript!");
+var sales = 123456789;
+var course = "TypeScript";
+var is_published = true;
+// any
+var level;
+level = 1;
+level = "a";
+// function
+function calcTax(income, taxYear) {
+    if (taxYear === void 0) { taxYear = 2022; }
+    var x;
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calcTax(6700, 2021);
+var employee = {
+    id: 1,
+    name: "",
+    retire: function (date) {
+        console.log("You retire at ".concat(date, "."));
+    }
+};
+employee.name = "jonas";
+// OOP interface
+var Account = /** @class */ (function () {
+    function Account(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    Account.prototype.deposit = function (amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        this.balance += amount;
+    };
+    return Account;
+}());
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,86 @@
-let sales: number = 123_456_789;
-let course: string = "TypeScript";
-let is_published = true;
+// let sales: number = 123_456_789;
+// let course: string = "TypeScript";
+// let is_published = true;
 
-// any
-let level;
-level = 1;
-level = "a";
+// // any
+// let level;
+// level = 1;
+// level = "a";
 
-// function
-function calcTax(income: number, taxYear: number = 2022): number {
-  let x;
-  if (taxYear < 2022) return income * 1.2;
-  return income * 1.3;
+// // function
+// function calcTax(income: number, taxYear: number = 2022): number {
+//   let x;
+//   if (taxYear < 2022) return income * 1.2;
+//   return income * 1.3;
+// }
+
+// calcTax(6700, 2021);
+
+// // object
+// // type aliases
+// type Employee = {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// };
+
+// let employee: Employee = {
+//   id: 1,
+//   name: "",
+//   retire(date) {
+//     console.log(`You retire at ${date}.`);
+//   },
+// };
+
+// employee.name = "jonas";
+
+// // OOP
+
+// class Account {
+//   id: number;
+//   owner: string;
+//   balance: number;
+
+//   constructor(id: number, owner: string, balance: number) {
+//     this.id = id;
+//     this.owner = owner;
+//     this.balance = balance;
+//   }
+
+//   deposit(amount: number): void {
+//     if (amount <= 0) throw new Error("Invalid amount");
+//     this.balance += amount;
+//   }
+// }
+
+// const account = new Account(1, "jonas", 100);
+
+// account.deposit(1);
+
+// interface
+
+interface Calendar {
+  name: string;
+  addEvent(): void;
+  removeEvent(): void;
 }
 
-calcTax(6700, 2021);
+interface CloudCalendar extends Calendar {
+  sync(): void;
+}
 
-// object
-// type aliases
-type Employee = {
-  readonly id: number;
-  name: string;
-  retire: (date: Date) => void;
-};
+class GoogleCalendar implements Calendar {
+  // 这三行等同于下面一行
+  // name: string;
+  // constructor(name: string) {
+  //   this.name = name;
+  // }
+  constructor(public name: string) {}
 
-let employee: Employee = {
-  id: 1,
-  name: "",
-  retire(date) {
-    console.log(`You retire at ${date}.`);
-  },
-};
-
-employee.name = "jonas";
+  addEvent(): void {
+    throw new Error("Method not implemented.");
+  }
+  removeEvent(): void {
+    throw new Error("Method not implemented.");
+  }
+}
